9335bfc887721a6b063d4140d8ecaaf67b8a0fc8
Alessandro <alessandroRetuerto@hotmail.com>
2023-11-07 18:52:05 -0500

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,10 +8,16 @@ import { BankInterceptor } from './bank.interceptor';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, provideFirebaseApp(() => initializeApp({"projectId":"banco-e6e96","appId":"1:787992739344:web:324e4c5dfa052b3ddc9495","storageBucket":"banco-e6e96.appspot.com","apiKey":"AIzaSyALYC6dbcRHYRwPqB8KH9FluBu1Q3BSlDo","authDomain":"banco-e6e96.firebaseapp.com","messagingSenderId":"787992739344"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, provideFirebaseApp(() => initializeApp({"projectId":"banco-e6e96","appId":"1:787992739344:web:324e4c5dfa052b3ddc9495","storageBucket":"banco-e6e96.appspot.com","apiKey":"AIzaSyALYC6dbcRHYRwPqB8KH9FluBu1Q3BSlDo","authDomain":"banco-e6e96.firebaseapp.com","messagingSenderId":"787992739344"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
